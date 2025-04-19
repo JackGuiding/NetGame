3cb906723ab6da2704f33c1219a8303553521db3
Jackutea <chenwansal1@163.com>
2025-04-19 16:51:40 +0800

    <feature> add: Sample

--- Assets/Client/Src_Runtime/ClientMain.cs
+++ Assets/Client/Src_Runtime/ClientMain.cs
@@ -1,17 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Telepathy;
 
 namespace GameClient {
 
     public class ClientMain : MonoBehaviour {
 
-        void Start() {
+        Client client;
 
+        bool isTearDown;
+
+        void Awake() {
+            int port = 12345;
+            int messageSize = 1024;
+            string ip = "127.0.0.1";
+            client = new Client(messageSize);
+
+            client.Connect(ip, port);
+            client.OnConnected += () => {
+                Debug.Log("[client]Connected");
+            };
+
+            client.OnData += (data) => {
+                Debug.Log("[client]Data: " + data.Count + " bytes");
+            };
+
+            client.OnDisconnected += () => {
+                Debug.Log("[client]Disconnected");
+            };
+
+            Debug.Log("[client]Connecting to " + ip + ":" + port);
+
+            Application.runInBackground = true;
         }
 
         void Update() {
+            if (client != null) {
+                client.Tick(10);
+            }
+        }
 
+        void OnDestroy() {
+            TearDown();
+        }
+
+        void OnApplicationQuit() {
+            TearDown();
+        }
+
+        void TearDown() {
+            if (isTearDown) return;
+            isTearDown = true;
+
+            if (client != null) {
+                client.Disconnect();
+            }
         }
 
     }
--- Assets/Server/Src_Runtime/ServerMain.cs
+++ Assets/Server/Src_Runtime/ServerMain.cs
@@ -1,17 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Telepathy;
 
 namespace GameServer {
 
     public class ServerMain : MonoBehaviour {
 
-        void Start() {
+        bool isTearDown;
+
+        Server server;
+
+        void Awake() {
+            int port = 12345;
+            int messageSize = 1024;
+            server = new Server(messageSize);
+            server.Start(port);
+
+            Debug.Log("[server]Listening on port " + port);
+
+            server.OnConnected += (connId, str) => {
+                Debug.Log("[server]Connected " + connId + ": " + str);
+                server.Send(connId, new byte[] { 1, 2, 3, 4, 5 });
+            };
+
+            server.OnData += (connId, data) => {
+                Debug.Log("[server]Data " + connId + ": " + data.Count + " bytes");
+                server.Send(connId, data);
+            };
+
+            server.OnDisconnected += (connId) => {
+                Debug.Log("[server]Disconnected " + connId);
+            };
+
+            Application.runInBackground = true;
 
         }
 
         void Update() {
+            if (server != null) {
+                server.Tick(10);
+            }
+        }
 
+        void OnDestroy() {
+            TearDown();
+        }
+
+        void OnApplicationQuit() {
+            TearDown();
+        }
+
+        void TearDown() {
+            if (isTearDown) return;
+            isTearDown = true;
+
+            if (server != null) {
+                server.Stop();
+            }
         }
 
     }
