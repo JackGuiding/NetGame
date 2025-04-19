b5e33f9f5b7f565a52884085129508851ce6ef09
Jackutea <chenwansal1@163.com>
2025-04-19 16:30:11 +0800

    ESTABLISH

---
+++ Assets/Server/Src_Runtime/ServerMain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameServer {
+
+    public class ServerMain : MonoBehaviour {
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+    }
+}
